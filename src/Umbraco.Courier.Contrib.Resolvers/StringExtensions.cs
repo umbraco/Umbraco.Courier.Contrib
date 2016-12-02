7eb77631c91a1d8c57cc84f90ded6f383d53e5ce
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-12-02 17:03:38 +0100

    JSON values with double quotes around them need to be properly escaped in the JSON string builder Some editors can have values stored in them that refer to document types that no longer exist, handled with a null check

---
+++ src/Umbraco.Courier.Contrib.Resolvers/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco.Courier.Contrib.Resolvers
+{
+    //From: http://stackoverflow.com/a/21455488/5018
+    public static class StringExtensions
+    {
+        public static string EscapeForJson(this string text)
+        {
+            var quoted = System.Web.Helpers.Json.Encode(text);
+            return quoted.Substring(1, quoted.Length - 2);
+        }
+    }
+}
