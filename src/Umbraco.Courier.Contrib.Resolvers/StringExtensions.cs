59798716815fc1e65faf14000b2db8675c8868e4
leekelleher <leekelleher@gmail.com>
2017-03-21 20:18:40 +0000

    Added DetectIsJson string extension method
    it is taken from Umbraco.Core, as it's marked as `internal`.

--- src/Umbraco.Courier.Contrib.Resolvers/StringExtensions.cs
+++ src/Umbraco.Courier.Contrib.Resolvers/StringExtensions.cs
@@ -1,12 +1,20 @@
 ﻿namespace Umbraco.Courier.Contrib.Resolvers
 {
-    //From: http://stackoverflow.com/a/21455488/5018
-    public static class StringExtensions
+    internal static class StringExtensions
     {
+        //From: http://stackoverflow.com/a/21455488/5018
         public static string EscapeForJson(this string text)
         {
             var quoted = System.Web.Helpers.Json.Encode(text);
             return quoted.Substring(1, quoted.Length - 2);
+        }
+
+        //From: https://github.com/umbraco/Umbraco-CMS/blob/dev-v7/src/Umbraco.Core/StringExtensions.cs#L123
+        public static bool DetectIsJson(this string input)
+        {
+            input = input.Trim();
+            return (input.StartsWith("{") && input.EndsWith("}"))
+                   || (input.StartsWith("[") && input.EndsWith("]"));
         }
     }
 }
